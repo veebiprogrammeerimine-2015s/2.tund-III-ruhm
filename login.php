1626d03a313f9a0caa04b245bab59a9c05945edc
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-09 13:33:04 +0300

    password error handling

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <?php
 	
 	// LOGIN.PHP
-
+	
+	// errori muutujad peavad igal juhul olemas olema 
 	$email_error = "";
+	$password_error = "";
 	
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -12,6 +14,21 @@
 		//kontrollin et e-post ei ole tühi
 		if ( empty($_POST["email"]) ) {
 			$email_error = "See väli on kohustuslik";
+		}
+		
+		//kontrollin, et parool ei ole tühi
+		if ( empty($_POST["password"]) ) {
+			$password_error = "See väli on kohustuslik";
+		} else {
+			
+			// kui oleme siia jõudnud, siis parool ei ole tühi
+			// kontrollin, et oleks vähemalt 8 sümbolit pikk
+			if(strlen($_POST["password"]) < 8) { 
+			
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+				
+			}
+			
 		}
 		
 	}
@@ -27,7 +44,7 @@
 		
 		<form action="login.php" method="post" >
 			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
-			<input name="password" type="password" placeholder="Parool"> <br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 			<input type="submit" value="Log in">
 		</form>
 	
