55f09ffe68e3e2555df96455223aa2266a9db79a
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-09 12:38:10 +0300

    added login page and form

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// LOGIN.PHP
+	
+?>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	
+	<h2>Log in</h2>
+		
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		<input type="submit" value="Log in">
+	
+	
+	<h2>Create user</h2>
+	
+</body>
+</html>
