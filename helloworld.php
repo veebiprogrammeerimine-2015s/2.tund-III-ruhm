f42681d70eac700693bb5771854b42488c8810db
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-09 11:32:57 +0300

    added logic

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,25 @@
 	
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+	
+	$age = 5;
+	
+	//testib loogikat, juhul kui vanus on väiksem 
+	//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
+	
+	// if'i sisse loogikatehe
+	if($age < 18) {
+		//tõene
+		echo "alaealine";
+		
+	} else {
+		//väär
+		echo "täisealine";
+	}
+	
+?>
+
+
+
